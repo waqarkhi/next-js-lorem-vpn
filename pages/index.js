c1f296997c66860944afccbddb1e26d67379e025
Waqar Ahmed <waqar.net.pk@gmail.com>
2022-12-26 14:39:11 +0500

    updated third party script issue

--- pages/index.js
+++ pages/index.js
@@ -9,20 +9,20 @@ import Price from '../section/Price'
 import Testimonial from '../section/Testimonial'
 import Why from '../section/Why'
 import Footer from '../section/Footer'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <div id="home">
+        <Script src="vendor/jquery/jquery-3.6.0.min.js" />
+        <Script strategy="lazyOnload" src="vendor/bootstrap/bootstrap.min.js" />
+        <Script strategy="lazyOnload" src="vendor/owl-carousel/owl.carousel.min.js" />
+        <Script strategy="lazyOnload" src="vendor/aos/aos.js" />
+        <Script strategy="lazyOnload" src="js/script.js" />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <title>Lorem VPN</title>
         <link rel="shortcut icon" href="images/favicon.webp" type="image/x-icon" />
-        
-        <script src="vendor/jquery/jquery-3.6.0.min.js"></script>
-        <script src="vendor/bootstrap/bootstrap.min.js" defer></script>
-        <script src="vendor/owl-carousel/owl.carousel.min.js"></script>
-        <script src="vendor/aos/aos.js" defer></script>
-        <script src="js/script.js" defer></script>
       </Head>
       <Header banner="true" />
       <About />
