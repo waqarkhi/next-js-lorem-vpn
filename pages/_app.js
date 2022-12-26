986067abb913f678a4de14814e51ca1c548d9862
Waqar Ahmed <waqar.net.pk@gmail.com>
2022-12-26 14:59:25 +0500

    stops animation and carousel

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <Script src="vendor/jquery/jquery-3.6.0.min.js" />
+        <Script strategy="lazyOnload" src="vendor/bootstrap/bootstrap.min.js" />
+        <Script strategy="lazyOnload" src="vendor/owl-carousel/owl.carousel.min.js" />
+        <Script strategy="lazyOnload" src="vendor/aos/aos.js" />
+        <Script strategy="lazyOnload" src="js/script.js" />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,10 @@ import Price from '../section/Price'
 import Testimonial from '../section/Testimonial'
 import Why from '../section/Why'
 import Footer from '../section/Footer'
-import Script from 'next/script'
 
 export default function Home() {
   return (
     <div id="home">
-        <Script src="vendor/jquery/jquery-3.6.0.min.js" />
-        <Script strategy="lazyOnload" src="vendor/bootstrap/bootstrap.min.js" />
-        <Script strategy="lazyOnload" src="vendor/owl-carousel/owl.carousel.min.js" />
-        <Script strategy="lazyOnload" src="vendor/aos/aos.js" />
-        <Script strategy="lazyOnload" src="js/script.js" />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <title>Lorem VPN</title>
